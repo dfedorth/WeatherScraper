680aa5e151849d5e28d361c60e861d78de91a7fc
dfedorth <dfedorth@Gh0stRAT.com>
2017-10-31 05:20:41 +0000

    Initial WeatherSpider commit

--- weatherspider/spiders/WeatherSpider.py
+++ weatherspider/spiders/WeatherSpider.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
+from scrapy import Request
 from scrapy.spiders import XMLFeedSpider
 
 
 class WeatherspiderSpider(XMLFeedSpider):
     name = 'WeatherSpider'
     allowed_domains = ['forecast.weather.gov']
-    start_urls = ['http://forecast.weather.gov/feed.xml']
+    url_template = "http://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}FcstType=digitalDWML"
     iterator = 'iternodes' # you can change this; see the docs
     itertag = 'item' # change it accordingly
+    target_latlons = [
+        ("38.6889", "-121.088"),
+    ]
+
+    def start_requests(self):
+        for (lat, lon) in target_latlons:
+            yield Request(url_template.format(lat=lat, lon=lon))
 
     def parse_node(self, response, selector):
         i = {}
